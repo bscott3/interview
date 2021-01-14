e1ee0169e3cf0b82042bbb3a1902d1077bbe482d
bscott3 <bryan.scott2@scotts.com>
2021-01-14 16:48:47 -0500

    setup proxy config and tested verified that http requests were working

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
-
-@app.route('/api/hello')
+@app.route("/api/hello", methods=["GET"])
 def hello_world():
-    return 'Hello World!'
+    return json.dumps(['Hello World!']), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="127.0.0.1", port=5000, debug=True)
